538169878572e845e5504426a680369f5deb9ace
DiosGonzalo <gdiosfiestas@gmail.com>
2026-01-29 22:50:38 +0100

    crear proyecto

---
+++ src/main/java/com/salesianostriana/punctual/PunctualApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.punctual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunctualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunctualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/punctual/PunctualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.punctual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunctualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
